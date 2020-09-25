1cf118e9df4b327575f8b0eb5c19f94c6a582b14
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-09-24 22:16:52 -0300

    Create DesafioStructs06.c

---
+++ Desafios/DesafioStructs06.c
@@ -0,0 +1,15 @@
+//Desafio: criar uma estrutura normal
+
+#include <stdio.h>
+
+struct Computador{
+  char CPU[20],GPU[20];
+  int memoriaRam;
+};
+
+int main(){
+
+  struct Computador configuracoes;
+
+  return 0;
+}
