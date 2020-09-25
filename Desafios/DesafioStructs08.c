e4cfdaa553ee1ede5e10b8822467c4f451338c50
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-09-24 22:16:59 -0300

    Create DesafioStructs08.c

---
+++ Desafios/DesafioStructs08.c
@@ -0,0 +1,16 @@
+//Desafio: criar uma estrutura com Union
+
+#include <stdio.h>
+
+union Aluno{
+  char faculdade[20],curso[20];
+  char escola[20]
+};
+// Usar condicionais.
+
+int main(){
+
+  union Aluno aluno;
+
+  return 0;
+}
