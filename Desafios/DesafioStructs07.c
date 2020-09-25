fdfc42d203da60d3c195f7f5634d2f2a165071d8
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-09-24 22:16:55 -0300

    Create DesafioStructs07.c

---
+++ Desafios/DesafioStructs07.c
@@ -0,0 +1,18 @@
+//Desafio: criar uma esstrutura com typedef
+
+#include <stdio.h>
+
+typedef struct{
+  char nome[20],cargoEmpresarial[20];
+  int idade;
+  float salario;
+  char sexo;
+} Funcionario;
+
+int main(){
+
+  Funcionario funcionario
+
+
+  return 0;
+}
