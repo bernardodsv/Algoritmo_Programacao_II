bc568d4768f80807dde3ded15c10034fba25f0ca
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-11-13 11:16:08 -0300

    Update DesafioFuncao01.c

--- Desafios/DesafioFuncao01.c
+++ Desafios/DesafioFuncao01.c
@@ -1,33 +1,28 @@
 /*
 Desafio: desenvolver um algoitmo para solicitar ao usuαrio
-2 nϊmeros reais e realizar as quatro operaηυes aritmιticas bαsicas com esses nϊmeros.
+2 numeros reais e realizar as quatro operaηυes aritmeticas bαsicas com esses nϊmeros.
 No final do programa, exibir os resultados obtidos. Utilize funηυes para cada operaηγo.
 */
 
 #include <stdio.h>
 
-void numeros();
-void exibirCalculos();
-int numero1,numero2;
+float n1,n2;
+void recebendoNumero(){
+    printf("Numero 1: \n");
+    scanf("%f",&n1);
+    printf("Numero 2: \n");
+    scanf("%f",&n2);
+}
+
+void calculadora(){
+    printf("\n");
+    printf("%.2f + %.2f = %.2f\n",n1,n2,(n1+n2));
+    printf("%.2f - %.2f = %.2f\n",n1,n2,(n1-n2));
+    printf("%.2f * %.2f = %.2f\n",n1,n2,(n1*n2));
+    printf("%.2f / %.2f = %.2f\n",n1,n2,(n1/n2));
+}
 
 int main(){
-
-  numeros();
-  exibirCalculos();
-
-  return 0;
-}
-
-void numeros(){
-  printf("Digite um numero inteiro: \n");
-  scanf("%d",&numero1);
-  printf("Digite um numero inteiro: \n");
-  scanf("%d",&numero2);
-}
-
-void exibirCalculos(){
-  printf("%d + %d = %d\n",numero1,numero2,(numero1+numero2));
-  printf("%d - %d = %d\n",numero1,numero2,(numero1-numero2));
-  printf("%d * %d = %d\n",numero1,numero2,(numero1*numero2));
-  printf("%d / %d = %d\n",numero1,numero2,(numero1/numero2));
+    recebendoNumero();
+    calculadora();
 }
